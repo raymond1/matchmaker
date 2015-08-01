28c949784081d44b2d8dd02d4941e52e609ea2a5
Raymond Leon <raymondleon@raymondleon.ca>
2015-07-31 23:20:12 -0400

    Corrected usage documentation to refer to the directory containing the go.sh file as the one where the go.sh script must be run. Previously, the documentation referred to the src directory instead.

--- go.sh
+++ go.sh
@@ -5,4 +5,4 @@ scalac org/json/*.java Matchmaker/*.scala
 cp Matchmaker/*.class .
 cp org/json/*.class .
 
-echo Type in "scala Matchmaker.Matchmaker" from the src directory to run the program. You will need the files listings.txt and products.txt in the src directory as well. The output file will be named results.txt.
+echo Type in "scala Matchmaker.Matchmaker" from the directory containing this file\(go.sh\) to run the program. You will need the files listings.txt and products.txt in the src directory as well. The output file will be named results.txt.
